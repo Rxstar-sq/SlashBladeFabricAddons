095b1b6056a69a4c037003fc1b4161e4857ef80d
殇泣乀 <87178985+Rxstar-sq@users.noreply.github.com>
2025-12-24 20:50:36 -0600

    统一tag方便其他模组通过tag管理

--- src/main/java/cn/sh1rocu/sfaddons/data/SFAddonsItemTagProvider.java
+++ src/main/java/cn/sh1rocu/sfaddons/data/SFAddonsItemTagProvider.java
@@ -1,6 +1,7 @@
 package cn.sh1rocu.sfaddons.data;
 
 import cn.mmf.energyblade.Energyblade;
+import cn.mmf.slashblade_addon.registry.SBAItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
@@ -16,7 +17,39 @@ public class SFAddonsItemTagProvider extends FabricTagProvider.ItemTagProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         this.getOrCreateTagBuilder(ItemTags.SWORDS).add(
-                Energyblade.FORGE_ENERGY_BLADE
+                Energyblade.FORGE_ENERGY_BLADE,
+                // SlashBlade Addon blades
+                SBAItems.SLASHBLADE_ADDON_AQUABLAZE,
+                SBAItems.SLASHBLADE_ADDON_BLUE,
+                SBAItems.SLASHBLADE_ADDON_CRIMSONCHERRY,
+                SBAItems.SLASHBLADE_ADDON_DARK_RAVEN,
+                SBAItems.SLASHBLADE_ADDON_FLUORESCENT_BAR,
+                SBAItems.SLASHBLADE_ADDON_FROSTY_CHERRY,
+                SBAItems.SLASHBLADE_ADDON_FROST_WOLF,
+                SBAItems.SLASHBLADE_ADDON_GREEN_MIST,
+                SBAItems.SLASHBLADE_ADDON_HF_MURASAMA,
+                SBAItems.SLASHBLADE_ADDON_KAMUY_FIRE,
+                SBAItems.SLASHBLADE_ADDON_KAMUY_LIGHTNING,
+                SBAItems.SLASHBLADE_ADDON_KAMUY_NONE,
+                SBAItems.SLASHBLADE_ADDON_KAMUY_WATER,
+                SBAItems.SLASHBLADE_ADDON_KATANA,
+                SBAItems.SLASHBLADE_ADDON_KIRISAYA,
+                SBAItems.SLASHBLADE_ADDON_LAEMMLE,
+                SBAItems.SLASHBLADE_ADDON_MOONLIGHT_CHERRY,
+                SBAItems.SLASHBLADE_ADDON_MURAKUMO,
+                SBAItems.SLASHBLADE_ADDON_NIHIL,
+                SBAItems.SLASHBLADE_ADDON_NIHILBX,
+                SBAItems.SLASHBLADE_ADDON_NIHILEX,
+                SBAItems.SLASHBLADE_ADDON_NIHILUL,
+                SBAItems.SLASHBLADE_ADDON_SNOW_CROW,
+                SBAItems.SLASHBLADE_ADDON_TACHI,
+                SBAItems.SLASHBLADE_ADDON_TBOEN,
+                SBAItems.SLASHBLADE_ADDON_TERRA_BLADE,
+                SBAItems.SLASHBLADE_ADDON_TOYAKO,
+                SBAItems.SLASHBLADE_ADDON_WANDERER,
+                SBAItems.SLASHBLADE_ADDON_WANDERER_HF,
+                SBAItems.SLASHBLADE_ADDON_YUKARI,
+                SBAItems.SLASHBLADE_ADDON_CIALLO
         );
     }
 }
